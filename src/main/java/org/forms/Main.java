f9643b5ce44a5bb5febd928fd5b0a29151cb03ac
artem <artempolevoj@yandex.ru>
2024-06-12 22:14:25 +0300

    update Main

--- src/main/java/org/forms/Main.java
+++ src/main/java/org/forms/Main.java
@@ -1,9 +1,18 @@
 package org.forms;
 
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
-
-        ReaderFile readerFile = new ReaderFile();
-        readerFile.reader("CHETRA.xlsx");
+        String fileNameExcel = "C:/Users/artem/Downloads/2024-06-12 Vizual'nyi osmotr bul'dozera ChETRA 1.2.xlsx";
+        String mainFolder = "C:/Users/artem/Desktop/Test";
+        FileRead fileRead = new FileRead(fileNameExcel);
+        Report report = fileRead.getReport();
+        Folder folder = new Folder(report, mainFolder);
+        WordFile wordFile = new WordFile(report, folder.getFolderName());
+   //     wordFile.createFile();
+//        System.out.println(report);
+//        Map<String, String> map = report.getAnswer();
+//        map.forEach((key, value) -> System.out.println(key + " - " + value));
     }
 }
