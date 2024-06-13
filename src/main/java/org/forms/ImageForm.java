90396c009115fc9701c193abad7191d61f82bc56
artem <artempolevoj@yandex.ru>
2024-06-13 07:01:37 +0300

    add ImageForm

---
+++ src/main/java/org/forms/ImageForm.java
@@ -0,0 +1,64 @@
+package org.forms;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.net.URI;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+public class ImageForm {
+    private final Report report;
+    private final String folder;
+    private Map<String, String> imageMap = new HashMap<>();
+
+    public ImageForm(Report report, String folder) {
+        this.report = report;
+        this.folder = folder;
+       // getImage();
+    }
+
+    private void getImage() {
+        Map<String, String> map = report.getAnswer();
+        String number;
+        for (String key : map.keySet()) {
+            if (key.contains("Фото") || key.equals("Общий вид машины")) {
+                if (key.contains("Фото")) {
+                    number = key.substring(0, 3);
+                } else {
+                    number = "O";
+                }
+                String[] arrImage = map.get(key).split(", ");
+
+                for (int i = 0; i < arrImage.length; i++) {
+
+                    try {
+                        Desktop d = Desktop.getDesktop();
+                        d.browse(new URI(arrImage[i]));
+                        TimeUnit.SECONDS.sleep(5);
+                        BufferedImage image = new Robot().createScreenCapture(new Rectangle(Toolkit.getDefaultToolkit().getScreenSize()));
+                        BufferedImage screen = image.getSubimage(200, 110, 1200, 750);
+                        String imageFileName = folder + "/" + number + "." + (i + 1) + ".jpeg";
+                        ImageIO.write(screen, "jpeg", new File(imageFileName));
+                        if (imageMap.containsKey(number)){
+                            String value = imageMap.get(number);
+                            value = value + ", " + imageFileName;
+                            imageMap.put(number, value);
+                        }else {
+                            imageMap.put(number, imageFileName);
+                        }
+
+                    } catch (Exception e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }
+        }
+    }
+    public Map<String, String> getImageMap(){
+        return imageMap;
+    }
+}
